66cdf77c71667a570681da436f442e3959edc073
Nikinggs <263999989+Nikinggs@users.noreply.github.com>
2026-08-15 21:55:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Academic Exchange | Login",
+  description: "Login page for Campus Marketplace",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="light">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
